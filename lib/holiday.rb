a023e9d43bc891abd947e029c33e588b9969a816
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-04 00:37:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 new_array = []
   holiday_hash.each do |key, holiday_names|
-    holiday_names
     puts ": #{holiday_names}"  << new_array
+    holiday_names.each do |values|
+      if values == "BBQ" << new_array
     binding.pry
   end 
 end 
