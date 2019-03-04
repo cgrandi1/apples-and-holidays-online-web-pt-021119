9de4a134862589656059fa2908b15330975cad26
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-04 00:28:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-return = [array]
+new_array = []
+holiday_hash.
 end
 
 
